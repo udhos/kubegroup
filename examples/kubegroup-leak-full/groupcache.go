5e6f707e732c7fbb6f0f35d9c8c2cd7159f335e6
udhos <udhos>
2023-12-28 00:31:04 -0300

    Test leak with groupcache workspace.

--- examples/kubegroup-leak-full/groupcache.go
+++ examples/kubegroup-leak-full/groupcache.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"log"
-	"net/http"
 	"os"
 	"time"
 
@@ -31,21 +30,25 @@ func startGroupcache(app *application) {
 		}
 	}
 
-	log.Printf("groupcache my URL: %s", myURL)
+	//log.Printf("groupcache my URL: %s", myURL)
 
-	pool := groupcache.NewHTTPPoolOpts(myURL, &groupcache.HTTPPoolOptions{})
+	ws := groupcache.NewWorkspace()
 
-	//
-	// start groupcache server
-	//
+	pool := groupcache.NewHTTPPoolOptsWithWorkspace(ws, myURL, &groupcache.HTTPPoolOptions{})
 
-	app.serverGroupCache = &http.Server{Addr: app.groupCachePort, Handler: pool}
+	/*
+		//
+		// start groupcache server
+		//
 
-	go func() {
-		log.Printf("groupcache server: listening on %s", app.groupCachePort)
-		err := app.serverGroupCache.ListenAndServe()
-		log.Printf("groupcache server: exited: %v", err)
-	}()
+		app.serverGroupCache = &http.Server{Addr: app.groupCachePort, Handler: pool}
+
+		go func() {
+			log.Printf("groupcache server: listening on %s", app.groupCachePort)
+			err := app.serverGroupCache.ListenAndServe()
+			log.Printf("groupcache server: exited: %v", err)
+		}()
+	*/
 
 	//
 	// start watcher for addresses of peers
@@ -56,12 +59,16 @@ func startGroupcache(app *application) {
 		GroupCachePort: app.groupCachePort,
 		//PodLabelKey:    "app",         // default is "app"
 		//PodLabelValue:  "my-app-name", // default is current PODs label value for label key
-		Debug: true,
+		Debug:  false,
+		Engine: kubegroup.NewKubeBogus(),
+		Errorf: func(format string, v ...any) {},
 	}
 
-	if app.engineBogus {
-		options.Engine = kubegroup.NewKubeBogus()
-	}
+	/*
+		if app.engineBogus {
+			options.Engine = kubegroup.NewKubeBogus()
+		}
+	*/
 
 	group, errGroup := kubegroup.UpdatePeers(options)
 	if errGroup != nil {
@@ -77,7 +84,7 @@ func startGroupcache(app *application) {
 	// https://talks.golang.org/2013/oscon-dl.slide#46
 	//
 	// 64 MB max per-node memory usage
-	app.cache = groupcache.NewGroup("files", app.groupCacheSizeBytes, groupcache.GetterFunc(
+	app.cache = groupcache.NewGroupWithWorkspace(ws, "files", app.groupCacheSizeBytes, groupcache.GetterFunc(
 		func(ctx context.Context, filePath string, dest groupcache.Sink) error {
 
 			log.Printf("cache miss, loading file: %s (ttl:%v)", filePath, app.groupCacheExpire)
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/udhos/kubegroup
 
-go 1.21.4
+go 1.21.5
 
 require (
 	github.com/mailgun/groupcache v1.3.0
